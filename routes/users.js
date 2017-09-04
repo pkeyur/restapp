acea372549a0205bf3c82de9932f275ddb5587f4
Keyur Patel <keyurpatel_kc@yahoo.com>
2017-09-04 16:39:51 -0700

    Connection to mysql established

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+//lists all existing users
+exports.listUsers = function(req, res) {
+    console.log('received request for listUsers');
+    req.getConnection(function(err, connection) {
+        connection.query('SELECT * FROM users', function(err, rows) {
+            if (err) {
+                console.log("Error Selecting : %s ", err);
+            }
+            var resUsr = [];
+            for (var usrIndex in rows) {
+                var usrObj = rows[usrIndex];
+                resUsr.push(usrObj);
+                console.log(usrObj);
+            }
+            res.json(resUsr);
+        });
+    });
+};
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+//get an existing user based on user_id
+exports.getID = function(req, res) {
+    console.log('received request for getID');
+    var id = req.params.id;
+    req.getConnection(function(err, connection) {
+        connection.query('SELECT * FROM users WHERE user_id=?', [id], function(err, rows) {
+            if (err) {
+                console.log("Error Selecting : %s ", err);
+            }
+            var resUsr = [];
+            for (var usrIndex in rows) {
+                var usrObj = rows[usrIndex];
+                resUsr.push(usrObj);
+                console.log(usrObj);
+            }
+            res.json(resUsr);
+        });
+    });
+};
