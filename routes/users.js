0530a1e9b0c924813864252ce0af96b491c43f16
Keyur Patel <keyurpatel_kc@yahoo.com>
2017-09-04 16:46:54 -0700

    Users module working

--- routes/users.js
+++ routes/users.js
@@ -36,3 +36,39 @@ exports.getID = function(req, res) {
         });
     });
 };
+
+//create a new user
+exports.createUser = function(req, res) {
+    console.log('received request for createUser');
+    var input = JSON.parse(JSON.stringify(req.body));
+    req.getConnection(function(err, connection) {
+        var data = {
+            user_name: input.uName,
+            user_address: input.uAddress
+        };
+        var query = connection.query("INSERT INTO users set ? ", data, function(err, rows) {
+            if (err) {
+                console.log("Error inserting : %s ", err);
+                res.sendStatus(400);
+            }
+            res.redirect('/users');
+        });
+    });
+};
+
+//delete an existing user
+exports.deleteUser = function(req, res) {
+    console.log('received request for deleteUser');
+    var id = req.params.id;
+    req.getConnection(function(err, connection) {
+        connection.query("DELETE FROM users WHERE user_id = ? ", id, function(err, rows) {
+            if (err) {
+                console.log("Error deleting : %s ", err);
+                res.sendStatus(400);
+            } else {
+                console.log('deleting successfully');
+                res.sendStatus(200);
+            }
+        });
+    });
+};
